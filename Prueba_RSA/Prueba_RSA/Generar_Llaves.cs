8e1e7dac14986ca73af139af12869f1a96a53eee
LuisPablo38 <63918165+LuisPablo38@users.noreply.github.com>
2023-10-24 22:47:35 -0600

    Add files via upload

---
+++ Prueba_RSA/Prueba_RSA/Generar_Llaves.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Prueba_RSA
+{
+    class Generar_Llaves
+    {
+        static Random random = new Random(); 
+
+        static bool EsPrimo(BigInteger primo)
+        {
+            if (primo <= 1)
+            {
+                return false;
+            }
+            if (primo <= 3)
+            {
+                return true; 
+            }
+            if (primo % 2 == 0 || primo % 3 == 0)
+            {
+                return false;
+            }
+            for (BigInteger i = 5; i*i <= primo; i+=6)
+            {
+                if (primo % i == 0 || primo % (i + 2) == 0)
+                    return false;
+            }
+            return true; 
+        }
+
+        public static BigInteger GenerarNumero(BigInteger maximo)
+        {
+            BigInteger numeroAleatorio;
+            do
+            {
+                byte[] randomByte = new byte[maximo.ToByteArray().Length];
+                random.NextBytes(randomByte);
+                numeroAleatorio = new BigInteger(randomByte) % maximo;
+
+            } while (!EsPrimo(numeroAleatorio));
+            return numeroAleatorio; 
+        }
+
+        public static BigInteger MultiNumeros(BigInteger numero1, BigInteger numero2)
+        {
+            BigInteger n = numero1 * numero2; 
+            return n; 
+        }
+
+        public static BigInteger GenerarZ(BigInteger numero1, BigInteger numero2)
+        {
+            BigInteger z = (numero1 - 1) * (numero2 - 1);
+            return z; 
+        }
+
+        public static BigInteger EncontrarCoprimo(BigInteger numero)
+        {
+            BigInteger coprimo;
+
+            do
+            {
+                coprimo = new BigInteger(random.Next(2, int.MaxValue)); 
+
+            } while (MCD(numero,coprimo) !=1);
+
+
+            return coprimo; 
+        }
+
+        public static BigInteger MCD(BigInteger a, BigInteger b)
+        {
+            while (b!= 0)
+            {
+                BigInteger temp = b;
+                b = a % b;
+                a = temp; 
+            }
+            return a; 
+        }
+
+        public static BigInteger ClavePrivada (BigInteger k, BigInteger z)
+        {
+            for (BigInteger l = 1; l<z; l++)
+            {
+                if((k*l)%z == 1)
+                {
+                    return l;
+                }
+            }
+
+            throw new Exception("No se encontro una clave valida");
+        }
+
+        //TXT a bigInteger 
+        public static BigInteger TxtAInteger (string texto)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(texto);
+            return new BigInteger(bytes);
+        }
+
+        //Integer a txt para comprobar si es lo mismo 
+        public static string BigIntegerToText(BigInteger number)
+        {
+            byte[] bytes = number.ToByteArray();
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        public static BigInteger Cifrado(BigInteger M, BigInteger K, BigInteger N)
+        {
+            BigInteger C;
+            C = BigInteger.ModPow(M, K, N);
+            return C; 
+        }
+
+        public static BigInteger DESCifrado(BigInteger M, BigInteger J, BigInteger N)
+        {
+            BigInteger DC;
+            DC = BigInteger.ModPow(M, J, N);
+            return DC;
+        }
+    }
+}
---
+++ Prueba_RSA/Prueba_RSA/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Numerics;
+using System.IO;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+namespace Prueba_RSA
+{
+    class Program
+    {
+        private static string contenido;
+        static void Main(string[] args)
+        {
+            string carpet = @"C:\Users\luisp\Desktop\Landivar\Cuarto año\Octavo Ciclo\Estructuras 2\Lab\InPutConv\inputs2";
+            Console.WriteLine("Ingrese el DPI para buscar la converzación");
+            string  identificador = Console.ReadLine();
+            try
+            {
+                string[] archivos = Directory.GetFiles(carpet, $"*{identificador}*.txt");
+                if (archivos.Length > 0)
+                {
+                    Console.WriteLine("Archivos encontrados con nombres similares:");
+                    foreach ( string archivo in archivos)
+                    {
+                        Console.WriteLine(archivo); //Mostrar en consola el direccion y nombre del archivo
+                        string nomArchCDesi = "Descifrado" + $"{Path.GetFileNameWithoutExtension(archivo)}_{identificador}.txt"; //Nombre que va a tener el nuevo archivo sin cifrar
+                        string rutaArchivoDesi = Path.Combine(@"C:\Users\luisp\Desktop\Landivar\Cuarto año\Octavo Ciclo\Estructuras 2\Lab\InPutConv\", nomArchCDesi); //Ruta y el nombre
+                        Console.WriteLine(" ");
+                        EscribiRArchi(archivo, rutaArchivoDesi); //Metodo para guardar (contenido, ruta del arhivo)  
+                    }  
+                }
+                else
+                {
+                    Console.WriteLine("No se encontraron archivos con nombres similares.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ocurrió un error: " + ex.Message);
+            }
+            BigInteger maximo = BigInteger.Pow(9, 4); // Valor máximo del rango ( ,num de ceros )
+            BigInteger numeroPrimoAleatorio1 = Generar_Llaves.GenerarNumero(maximo); //P
+            BigInteger numeroPrimoAleatorio2 = Generar_Llaves.GenerarNumero(maximo); //Q
+            BigInteger ResMulti = Generar_Llaves.MultiNumeros(numeroPrimoAleatorio1, numeroPrimoAleatorio2); // Esta es mi variable N
+            BigInteger ResMulti2Z = Generar_Llaves.GenerarZ(numeroPrimoAleatorio1, numeroPrimoAleatorio2);
+            BigInteger Coprimo = Generar_Llaves.EncontrarCoprimo(ResMulti2Z); //K
+            BigInteger Key_Privada = Generar_Llaves.ClavePrivada(Coprimo, ResMulti2Z); //J
+            BigInteger M = Generar_Llaves.TxtAInteger(contenido);
+            BigInteger C = Generar_Llaves.Cifrado(M, Coprimo, ResMulti); //Metodo para aplicar RSA
+            /*Console.WriteLine("Número primo aleatorio menor a  " + maximo + " : " + numeroPrimoAleatorio1);
+            Console.WriteLine("Número primo 2 aleatorio menor a  " + maximo + " : " + numeroPrimoAleatorio2);
+            Console.WriteLine("Resultado multiplicacion N : " + ResMulti);
+            Console.WriteLine("Resultado Z : " + ResMulti2Z);
+            Console.WriteLine("Coprimo K : " + Coprimo);
+            Console.WriteLine("Llave privada : " + Key_Privada);
+            Console.WriteLine(" " );*/
+            //Console.WriteLine("valor de M" + M); 
+            Console.WriteLine("Mensaje cifrado: " + C);
+            Console.WriteLine(" ");
+            string opcion; 
+            Console.WriteLine(" ¿Desea validar que la conversacion no a sido modificada?  ");
+            opcion = Console.ReadLine(); 
+            if (opcion == "Si" || opcion == "si" || opcion == "SI")
+            {
+                string arrego  = Generar_Llaves.BigIntegerToText(M);
+                string[] archivos2 = Directory.GetFiles(carpet, $"*{identificador}*.txt");
+                if (archivos2.Length > 0)
+                {
+                    foreach (string archivo in archivos2)
+                    {
+                        string nomArchCDesi = "Descifrado" + $"{Path.GetFileNameWithoutExtension(archivo)}_{identificador}.txt"; //Nombre que va a tener el nuevo archivo sin cifrar
+                        string rutaArchivoDesi = Path.Combine(@"C:\Users\luisp\Desktop\Landivar\Cuarto año\Octavo Ciclo\Estructuras 2\Lab\InPutConv\", nomArchCDesi); //Ruta y el nombre
+                        Console.WriteLine(" ");
+                        EscribiRArchi2(archivo, rutaArchivoDesi); //Metodo para guardar (contenido, ruta del arhivo)  
+                        Console.WriteLine(" ");
+                        string hash2 = CrearHash(arrego);
+                        string hash1 = CrearHash(contenido);
+                        if (hash2 == hash1)
+                        {
+                            Console.WriteLine("La conversacion no se ha modificado ");
+                        }
+                        else
+                        {
+                            Console.WriteLine("La conversacion fue comprometida");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No se encontraron archivos con nombres similares.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fin de la verificación"); 
+            }  
+        }
+        static void EscribiRArchi(string rutaOrigen, string rutaDestino)
+        {
+            try
+            {
+                contenido = File.ReadAllText(rutaOrigen);
+                CrearHash(contenido); //Crear el hash  del contenido del archivo 
+                File.WriteAllText(rutaDestino,contenido);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error al escribir en el archivo");
+            }
+        }
+        static string CrearHash (string mensaje)
+        {
+            using (SHA256 sHA256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(mensaje);
+                byte[] hashBytes = sHA256.ComputeHash(inputBytes);
+                // Convierte los bytes del hash en una cadena hexadecimal
+                string hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+                Console.WriteLine("Firma digital: " + hash);
+                Console.WriteLine("");
+                return hash;
+            }
+        }
+        static void EscribiRArchi2(string rutaOrigen, string rutaDestino)
+        {
+            try
+            {
+                contenido = File.ReadAllText(rutaOrigen);
+                
+                File.WriteAllText(rutaDestino, contenido);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error al escribir en el archivo");
+            }
+        }
+    }
+}
